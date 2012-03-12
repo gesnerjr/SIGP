d36e80e2e072ca5db7c45471f51b867f628021ec
Guilherme Nogueira <guilherme@nirev.org>
2012-03-11 22:31:30 -0300

    adding common javascript

---
+++ WebContent/js/utils.js
@@ -0,0 +1,26 @@
+$(document).ready(function()
+{   
+    $(".monthPicker").datepicker({
+        dateFormat: 'mm/yy',
+        changeMonth: true,
+        changeYear: true,
+        showButtonPanel: true,
+
+        onClose: function(dateText, inst) {
+            var month = $("#ui-datepicker-div .ui-datepicker-month :selected").val();
+            var year = $("#ui-datepicker-div .ui-datepicker-year :selected").val();
+            $(".monthResult").val(month);
+            $(".yearResult").val(year);
+            $(this).val($.datepicker.formatDate('mm/yy', new Date(year, month, 1)));
+        }
+    });
+
+    $(".monthPicker").focus(function () {
+        $(".ui-datepicker-calendar").hide();
+        $("#ui-datepicker-div").position({
+            my: "center top",
+            at: "center bottom",
+            of: $(this)
+        });
+    });
+});
